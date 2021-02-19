de766fa24fd28a79acaf6079d842ade75ae80bd0
roblivesinottawa <tech.rob@icloud.com>
2021-02-19 16:01:23 -0500

    "second commit"

--- doubly_list.py
+++ doubly_list.py
@@ -70,3 +70,91 @@ def insert_after_item(self, x, data):
             if n.nref is not None:
                 n.nref.prev = new_node
             n.nref = new_node
+
+
+def insert_before_item(self, x, data):
+    if self.start_node is None:
+        print("List is empty")
+        return
+    else:
+        n = self.start_node
+        while n is not None:
+            if n.item == x:
+                break
+            n = n.nref
+        if n is None:
+            print("item not in the list")
+        else:
+            new_node = Node(data)
+            new_node.nref = n
+            new_node.pref = n.pref
+            if n.pref is not None:
+                n.pref.nref = new_node
+            n.pref = new_node
+
+
+def traverse_list(self):
+    if self.start_node is None:
+        print("List has no element")
+        return
+    else:
+        n = self.start_node
+        while n is not None:
+            print(n.item , " ")
+            n = n.nref
+
+def delete_at_start(self):
+    if self.start_node is None:
+        print("The list has no element to delete")
+        return 
+    if self.start_node.nref is None:
+        self.start_node = None
+        return
+    self.start_node = self.start_node.nref
+    self.start_prev = None
+
+
+def delete_at_end(self):
+    if self.start_node is None:
+        print("The list has no element to delete")
+        return 
+    if self.start_node.nref is None:
+        self.start_node = None
+        return
+    n = self.start_node
+    while n.nref is not None:
+        n = n.nref
+    n.pref.nref = None
+
+
+def delete_element_by_value(self, x):
+    if self.start_node is None:
+        print("The list has no element to delete")
+        return 
+    if self.start_node.nref is None:
+        if self.start_node.item == x:
+            self.start_node = None
+        else:
+            print("Item not found")
+        return 
+
+    if self.start_node.item == x:
+        self.start_node = self.start_node.nref
+        self.start_node.pref = None
+        return
+
+    n = self.start_node
+    while n.nref is not None:
+        if n.item == x:
+            break
+        n = n.nref
+    if n.nref is not None:
+        n.pref.nref = n.nref
+        n.nref.pref = n.pref
+    else:
+        if n.item == x:
+            n.pref.nref = None
+        else:
+            print("Element not found")
+
+     
